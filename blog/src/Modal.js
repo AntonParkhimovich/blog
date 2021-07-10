ab9a232db6d0edc4a3cc602aa9f8bedcf10487a9
AntonParkhimovich <pad1907@gmail.com>
2021-07-10 14:56:00 +0000

    feat: add modal

---
+++ blog/src/Modal.js
@@ -0,0 +1,12 @@
+import React, {useState}from 'react'
+const Modal =(props)=>{
+    const 
+    return(
+        <div className={'modal'}>
+            <div className={'modal-body'}>
+                <h2 className={'modal-title'}>{'Please login'}</h2> 
+                <input type ={'text'} placeholder ={'login'}/>
+                <input type={'password'} placeholder ={'Password'}/>
+            </div>
+        </div>
+    )
